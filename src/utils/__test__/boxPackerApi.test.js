d4bf17871eb08eabdc6df5735686260feda7df20
David Kidd <dkidd@northamericanweb.com>
2019-07-03 02:28:57 -0500

    Added tests for attemptPack function

---
+++ src/utils/__test__/boxPackerApi.test.js
@@ -0,0 +1,46 @@
+import {attemptPack} from "../boxPackerAPI";
+
+global.fetch = jest.fn(() => {
+    return new Promise(resolve => resolve(new Response(JSON.stringify({label:'response-data'})),
+        reject => reject()));
+});
+
+describe('boxPackerApi', () => {
+    it('calls fetch with the right parameters', () => {
+        const request = {label: 'request-data'};
+        const init = {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+            referrer: 'boxpackerui',
+            body: JSON.stringify(request)
+        };
+
+        const response = attemptPack(request);
+
+        expect(fetch).toHaveBeenCalledWith(expect.any(String), init);
+
+        response.then(response => {
+            expect(response).toEqual({
+                loadingTime: expect.any(Number),
+                request: {label: 'request-data'},
+                response: {label: 'response-data'}
+            });
+        });
+    });
+
+    it('handles errors correctly', () => {
+        fetch.mockReturnValueOnce(
+            new Promise(resolve => resolve(new Response(JSON.stringify({message:'error-message'}))),
+            reject => reject()));
+
+        const response = attemptPack({label: 'request-data'});
+
+        response.catch( (error) => {
+            expect(error).toEqual(new Error('error-message'));
+        });
+    });
+});
