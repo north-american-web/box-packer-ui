1bac5520eb416716abc13d322a3ee04dc67e4383
David Kidd <dkidd@northamericanweb.com>
2019-07-05 14:30:32 -0500

    Migration to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ReactGA from 'react-ga';
+import ReactGA, {EventArgs} from 'react-ga';
 
 let trackableEventHandler;
 if( process.env.REACT_APP_GA_ID ){
     ReactGA.initialize(process.env.REACT_APP_GA_ID);
     ReactGA.pageview(window.location.pathname + window.location.search);
-    trackableEventHandler = ({ category, action, label }) => {
+    trackableEventHandler = ({ category, action, label }: EventArgs) => {
         ReactGA.event({ category, action, label })
     }
 }
